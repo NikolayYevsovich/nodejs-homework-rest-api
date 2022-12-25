3b29a7ec4939cb07e55b9cdd1f3e7d4d501e6408
NikolayYevsovich <yevsovich.nick@gmail.com>
2022-12-25 16:18:50 +0200

    MongoDB

---
+++ controllers/contacts/removeById.js
@@ -0,0 +1,15 @@
+const HttpError = require("../../helpers");
+const Contact = require("../../models/contact");
+
+const removeById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
+  if (!result) {
+    throw HttpError(404, "Not found");
+  }
+  res.json({
+    message: "Delete success",
+  });
+};
+
+module.exports = removeById;
