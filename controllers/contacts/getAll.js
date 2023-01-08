edfdd752d57fdff6644287c354503a6e95f0328d
NikolayYevsovich <yevsovich.nick@gmail.com>
2023-01-08 20:45:07 +0200

    hw04-auth

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,8 +1,13 @@
-// const contacts = require("../../models/contacts");
 const Contact = require("../../models/contact");
 
-const getAll = async (req, res, next) => {
-  const result = await Contact.find();
+const getAll = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "name email");
   res.json(result);
 };
 
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,27 +1,34 @@
 const express = require("express");
 const ctrl = require("../../controllers/contacts");
 const { ctrlWrapper } = require("../../helpers");
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const schemas = require("../../schemas/contacts");
 
 const router = express.Router();
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", authenticate, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", authenticate, ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrlWrapper(ctrl.add)
+);
 
 router.put(
   "/:contactId",
+  authenticate,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateById)
 );
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId", authenticate, ctrlWrapper(ctrl.removeById));
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   validateBody(schemas.changeFavorite),
   ctrlWrapper(ctrl.updateFavorite)
 );
